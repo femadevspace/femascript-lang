50e7865480253618a2730cf9ade8d36ff349be68
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-25 10:27:37 -0300

    feat: ✨ adicionar o índice ao diretório lexer

---
+++ src/grammar/lexer/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Automatically register all tokens and fragments
+ */
+import "./fragments";
+import "./tokens";
+
+export * from "./tokens";
+
+export * from "./tokens.registry";
+
+export * from "./lexer";
