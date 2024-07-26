966f3559517469c27c8c4c37571e1f175942fba3
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-26 18:25:53 -0300

    feat: ✨ adicionar sinalizador para pular validações ao analisador léxico
    Aumento de performance em ambientes de produção.

--- src/grammar/lexer/lexer.ts
+++ src/grammar/lexer/lexer.ts
@@ -4,6 +4,7 @@ import { tokensRegistry } from "../lexer";
 const lexer = new Lexer(tokensRegistry.getAllTokens(), {
   ensureOptimizations: true,
   positionTracking: "onlyOffset",
+  skipValidations: !!process && process.env.NODE_ENV === "production",
 });
 
 export { lexer };
