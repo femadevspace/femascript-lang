1a5bc09d0ae6e57be90e0f281789df743ff9a8f1
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-26 18:30:15 -0300

    feat: ✨ adicionar o índice principal

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import type { CstNode } from "chevrotain";
+import { lexer } from "./grammar/lexer";
+import { AlgoritmoLanguageParser } from "./grammar/parser";
+import { handleLexErrors, handleParserErrors } from "./utils";
+
+const parser = new AlgoritmoLanguageParser();
+
+const visitor = parser.getBaseCstVisitorConstructor();
+const visitorWithDefaults = parser.getBaseCstVisitorConstructorWithDefaults();
+
+const parse = (inputText: string, entryPoint = "program") => {
+  const { tokens, errors } = lexer.tokenize(inputText);
+
+  handleLexErrors(errors);
+
+  parser.input = tokens;
+
+  if (!(entryPoint in parser))
+    throw new Error(`Invalid entry point: ${entryPoint}`);
+
+  const rule = parser[entryPoint as keyof AlgoritmoLanguageParser];
+
+  if (typeof rule !== "function")
+    throw new Error(`Invalid entry point: ${entryPoint}`);
+
+  const cst = (rule.bind(parser) as Function)() as CstNode;
+
+  handleParserErrors(parser.errors);
+
+  return cst;
+};
+
+export default {
+  parse,
+  visitor,
+  visitorWithDefaults,
+};
