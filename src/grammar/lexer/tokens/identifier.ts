344be19c93c6fde50e2c3e4921201edec46b782d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-27 01:02:47 -0300

    refactor: ✏️ acrescentar ".other" ao token de identificadores

--- src/grammar/lexer/tokens/identifier.ts
+++ src/grammar/lexer/tokens/identifier.ts
@@ -3,5 +3,5 @@ import { add as TOKEN } from "../tokens.registry";
 export const Identifier = TOKEN({
   name: "Identifier",
   pattern: /[a-zA-Z$_][a-zA-Z0-9$_]*/,
-  textmateScope: "variable",
+  textmateScope: "variable.other",
 });
