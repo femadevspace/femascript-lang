9bdf98dd830fb014e1291f82cefafd7ae9cf5d69
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-27 03:45:04 -0300

    refactor: ♻️ adicionar o tipo `ParserEntryPoint` ao segundo parâmetro da função `parse`
    Esta alteração elimina a necessidade de validar manualmente o argumento `entryPoint`, pois ele agora aceita apenas as regras definidas no analisador sintático.

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,21 @@ import { lexer } from "@/grammar/lexer";
 import { AlgoritmoLanguageParser } from "@/grammar/parser";
 import { handleLexErrors, handleParserErrors } from "@/utils";
 import type { CstNode } from "chevrotain";
+import type { ParserEntryPoint } from "./types/cst";
 
 const parser = new AlgoritmoLanguageParser();
 
 const visitor = parser.getBaseCstVisitorConstructor();
 const visitorWithDefaults = parser.getBaseCstVisitorConstructorWithDefaults();
 
-const parse = (inputText: string, entryPoint = "program") => {
+const parse = (inputText: string, entryPoint: ParserEntryPoint = "program") => {
   const { tokens, errors } = lexer.tokenize(inputText);
 
   handleLexErrors(errors);
 
   parser.input = tokens;
 
-  if (!(entryPoint in parser))
-    throw new Error(`Invalid entry point: ${entryPoint}`);
-
-  const rule = parser[entryPoint as keyof AlgoritmoLanguageParser];
-
-  if (typeof rule !== "function")
-    throw new Error(`Invalid entry point: ${entryPoint}`);
-
+  const rule = parser[entryPoint as keyof AlgoritmoLanguageParser] as Function;
   const cst = (rule.bind(parser) as Function)() as CstNode;
 
   handleParserErrors(parser.errors);
