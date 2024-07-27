b954942fa392e659685dbb1d0e5dd8bc71e73e28
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-27 04:56:42 -0300

    refactor: ♻️ atualizar a função `parse` para retornar o tipo de nó da CST conforme o `entryPoint` correspondente

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,29 @@
 import { lexer } from "@/grammar/lexer";
 import { AlgoritmoLanguageParser } from "@/grammar/parser";
+import type { CstNodeTypes, ParserEntryPoint } from "@/types/cst";
 import { handleLexErrors, handleParserErrors } from "@/utils";
-import type { CstNode } from "chevrotain";
-import type { ParserEntryPoint } from "./types/cst";
 
 const parser = new AlgoritmoLanguageParser();
 
 const visitor = parser.getBaseCstVisitorConstructor();
 const visitorWithDefaults = parser.getBaseCstVisitorConstructorWithDefaults();
 
-const parse = (inputText: string, entryPoint: ParserEntryPoint = "program") => {
+const parse = <EntryPoint extends ParserEntryPoint = "program">(
+  inputText: string,
+  entryPoint: EntryPoint = "program" as EntryPoint
+) => {
   const { tokens, errors } = lexer.tokenize(inputText);
 
   handleLexErrors(errors);
 
   parser.input = tokens;
 
-  const rule = parser[entryPoint as keyof AlgoritmoLanguageParser] as Function;
-  const cst = (rule.bind(parser) as Function)() as CstNode;
+  const rule = parser[entryPoint as keyof typeof parser] as Function;
+  const cst = rule.bind(parser)();
 
   handleParserErrors(parser.errors);
 
-  return cst;
+  return cst as CstNodeTypes[EntryPoint];
 };
 
 export default {
