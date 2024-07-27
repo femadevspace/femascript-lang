afd3d6a8dff5504ad3eec793d91231c447ef05a6
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-27 01:41:19 -0300

    refactor!: ♻️ refatorar o registrador dos tokens removendo o uso de singleton
    Além de não ser mais um singleton, esta alteração separa a responsabilidade de lidar com tokens escopados com TextMate, melhorando a modularidade do código.
    
    BREAKING CHANGE: Os arquivos de registro individuais de tokens precisam ser ajustados para se adequar a esta mudança.

---
+++ src/grammar/lexer/tokens-registry.ts
@@ -0,0 +1,24 @@
+import { createToken, type ITokenConfig, type TokenType } from "chevrotain";
+import {
+  handleTextmateScope,
+  type MaybeTextmateScoped,
+} from "../../utils/textmate";
+
+export type Token = TokenType & MaybeTextmateScoped;
+export type TokenConfig = ITokenConfig & MaybeTextmateScoped;
+
+const tokensRegistry = new Set<Token>();
+
+const registerToken = (token: Token) => {
+  tokensRegistry.add(token);
+};
+
+const createAndRegisterToken = (tokenConfig: TokenConfig) =>
+  registerToken({
+    ...createToken(tokenConfig),
+    ...handleTextmateScope(tokenConfig),
+  });
+
+const getTokens = () => tokensRegistry.values();
+
+export { createAndRegisterToken, getTokens, registerToken };
---
+++ src/utils/textmate.ts
@@ -0,0 +1,10 @@
+import type { TokenConfig } from "../grammar/lexer";
+
+export type WithTextmateScope = { textmateScope: string };
+export type SkipTextmateScope = { skipTextmateScope: true };
+export type MaybeTextmateScoped = WithTextmateScope | SkipTextmateScope;
+
+export const handleTextmateScope = (config: TokenConfig): MaybeTextmateScoped =>
+  "skipTextmateScope" in config
+    ? { skipTextmateScope: true }
+    : { textmateScope: config.textmateScope };
