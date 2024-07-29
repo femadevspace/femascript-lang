48dbfd547fa8c7b688d68359ae6c0b6d54ca8723
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-29 06:50:06 -0300

    fix: 🐛 corrigir a definição dos tokens de tipos primitivos

--- src/grammar/lexer/tokens/types.ts
+++ src/grammar/lexer/tokens/types.ts
@@ -3,33 +3,44 @@ import { createAndRegisterToken as TOKEN } from "../tokens-registry";
 
 const textmateScope = "support.type.primitive";
 
+export const PrimitiveTypes = TOKEN({
+  name: "PrimitiveTypes",
+  pattern: Lexer.NA,
+  skipTextmateScope: true,
+});
+
 export const RealType = TOKEN({
   name: "RealType",
   pattern: /\b(real|reais)\b/,
+  categories: [PrimitiveTypes],
   textmateScope,
 });
 
 export const IntegerType = TOKEN({
   name: "IntegerType",
   pattern: /\b(int|integer|integers|inteiro|inteiros)\b/,
+  categories: [PrimitiveTypes],
   textmateScope,
 });
 
 export const CharType = TOKEN({
   name: "CharType",
   pattern: /\b(char|character|caractere)\b/,
+  categories: [PrimitiveTypes],
   textmateScope,
 });
 
 export const TextType = TOKEN({
   name: "TextType",
   pattern: /\b(string|text|texto)\b/,
+  categories: [PrimitiveTypes],
   textmateScope,
 });
 
 export const BooleanType = TOKEN({
   name: "BooleanType",
   pattern: /\b(bool|boolean|booleano|logical|logic|lógico|logico)\b/,
+  categories: [PrimitiveTypes],
   textmateScope,
 });
 
@@ -43,11 +54,4 @@ export const OfType = TOKEN({
   name: "Of",
   pattern: /\b(of|de)\b/,
   textmateScope,
-});
-
-export const PrimitiveTypes = TOKEN({
-  name: "PrimitiveTypes",
-  pattern: Lexer.NA,
-  categories: [RealType, IntegerType, CharType, TextType, BooleanType],
-  skipTextmateScope: true,
 });
