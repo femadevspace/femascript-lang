b232d030c16d04d388251c86e4c7be981797644d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-07-31 17:59:03 -0300

    feat: ✨ atualizar a proriedade `textmateScope` para conter o tipo `TextmatePattern` conforme a gramática Textmate
    Esta alteração alinha a implementação com a gramática padrão do Textmate, garantindo melhor compatibilidade e funcionamento.

--- src/utils/textmate.ts
+++ src/utils/textmate.ts
@@ -1,6 +1,28 @@
 import type { TokenConfig } from "@/grammar/lexer";
 
-export type WithTextmateScope = { textmateScope: string };
+/**
+ * This follows the schema of Textmate's grammar JSON.
+ * The schema can be found at https://raw.githubusercontent.com/martinring/tmlanguage/master/tmlanguage.json
+ *
+ * @property `repository` - If there is no explicit repository specified,
+ * it defaults to the word that precedes the first dot (".") in the `name` property.
+ */
+export type TextmatePattern = {
+  repository?: string;
+  name: string;
+  patterns?: TextmatePattern[];
+  match?: string;
+  comment?: string;
+  disabled?: 0 | 1;
+  include?: string;
+  contentName?: string;
+  begin?: string;
+  end?: string;
+  while?: string;
+  applyEndPatternLast?: 0 | 1;
+};
+
+export type WithTextmateScope = { textmateScope: string | TextmatePattern };
 export type SkipTextmateScope = { skipTextmateScope: true };
 export type MaybeTextmateScoped = WithTextmateScope | SkipTextmateScope;
 
