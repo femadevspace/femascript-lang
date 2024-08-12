cccf8f8a835ebe0ce7a39a8a5a55231481686cf0
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-12 17:03:28 -0300

    build: 🏗️ adicionar o script do esbuild para lidar com CommonJS

---
+++ esbuild.mjs
@@ -0,0 +1,58 @@
+// @ts-check
+import * as esbuild from "esbuild";
+
+const watch = process.argv.includes("--watch");
+const minify = process.argv.includes("--minify");
+
+const success = watch ? "Watch build succeeded" : "Build succeeded";
+
+const pad = (i) => i.toString().padStart(2, "0");
+
+function getTime() {
+  const date = new Date();
+  const hours = pad(date.getHours());
+  const minutes = pad(date.getMinutes());
+  const seconds = pad(date.getSeconds());
+
+  return `[${`${hours}h${minutes}:${seconds}`}] `;
+}
+
+/** @type {import('esbuild').Plugin[]} */
+const plugins = [
+  {
+    name: "watch-plugin",
+    setup: (build) =>
+      build.onEnd((result) =>
+        result.errors.length === 0 ? console.log(getTime() + success) : null
+      ),
+  },
+];
+
+const ctx = await esbuild.context({
+  entryPoints: ["src/extension.ts"],
+  outdir: "extension",
+  bundle: true,
+  target: "ES2017",
+  loader: { ".ts": "ts" },
+  external: ["vscode"],
+  platform: "node",
+  sourcemap: !minify,
+  minify,
+  plugins,
+
+  /**
+   * VSCode's extension host is still using cjs,
+   * so it's needed to use cjs to avoid any issues.
+   */
+  format: "cjs",
+  outExtension: {
+    ".js": ".cjs",
+  },
+});
+
+if (watch) {
+  await ctx.watch();
+} else {
+  await ctx.rebuild();
+  ctx.dispose();
+}
