97a04082c78468f4cac69f8ea6499a302e4de4e7
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-17 16:27:40 -0300

    feat: ✨ adicionar utilitário para lidar com aninhamento de objetos e arrays

---
+++ src/utils/nested.ts
@@ -0,0 +1,22 @@
+export type NestedArray<T> = Array<T | NestedArray<T>>;
+
+export type DeepPartial<T> = {
+  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
+
+export const mergeDeep = <T>(target: T, source: DeepPartial<T>): T => {
+  const output = { ...target };
+
+  for (const key in source) {
+    if (source.hasOwnProperty(key)) {
+      const value = source[key];
+
+      if (value && typeof value === "object" && !Array.isArray(value)) {
+        if (!output[key]) (output[key] as any) = {};
+        output[key] = mergeDeep(output[key], value);
+      } else (output[key] as any) = value;
+    }
+  }
+
+  return output;
+};
