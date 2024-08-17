8b11d8ba69b4f2b8563d0f091949b16ea496d6fd
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-17 15:58:03 -0300

    refactor: ♻️ mover os utilitários de análise sintática (parsing) para seu próprio arquivo

--- src/grammar/index.ts
+++ src/grammar/index.ts
@@ -1,33 +1,3 @@
-import { lexer } from "@/grammar/lexer";
-import { FemaScriptLanguageParser, type EntryPoint } from "@/grammar/parser";
-import type { CstNodeTypes } from "@/types/cst";
-import { handleLexErrors, handleParserErrors } from "@/utils";
+export * from "./lexer";
 
-const parser = new FemaScriptLanguageParser();
-
-const BaseVisitor = parser.getBaseCstVisitorConstructor();
-const BaseVisitorWithDefaults =
-  parser.getBaseCstVisitorConstructorWithDefaults();
-
-const parse = <Entry extends EntryPoint = "algorithm">(
-  inputText: string,
-  entryPoint: Entry = "algorithm" as Entry
-) => {
-  const { tokens, errors, groups } = lexer.tokenize(inputText);
-
-  handleLexErrors(errors);
-
-  parser.input = tokens;
-
-  const cst = parser[entryPoint]() as CstNodeTypes[Entry];
-
-  handleParserErrors(parser.errors);
-
-  return { cst, groups };
-};
-
-export default {
-  parse,
-  BaseVisitor,
-  BaseVisitorWithDefaults,
-};
+export * from "./parser";
---
+++ src/grammar/parser/parse-helper.ts
@@ -0,0 +1,23 @@
+import { lexer } from "@/grammar";
+import type { CstNodeTypes } from "@/types/cst";
+import { handleLexErrors, handleParserErrors } from "@/utils";
+import { FemaScriptLanguageParser, type EntryPoint } from "./parser";
+
+const parser = new FemaScriptLanguageParser();
+
+export const parse = <Entry extends EntryPoint = "algorithm">(
+  inputText: string,
+  entryPoint: Entry = "algorithm" as Entry
+) => {
+  const { tokens, errors, groups } = lexer.tokenize(inputText);
+
+  handleLexErrors(errors);
+
+  parser.input = tokens;
+
+  const cst = parser[entryPoint]() as CstNodeTypes[Entry];
+
+  handleParserErrors(parser.errors);
+
+  return { cst, groups };
+};
