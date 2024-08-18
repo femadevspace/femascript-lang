84c36631f4104c7b4f8206f8d01af5f6cc2e39e5
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 03:04:34 -0300

    feat: ✨ adicionar visitor para os statements de iteração

--- src/formatter/visitors/statements-control-flow.ts
+++ src/formatter/visitors/statements-control-flow.ts
@@ -1,3 +1,21 @@
+import * as cst from "@/types/cst";
 import { FemaScriptFormatterVisitor } from "../formatter";
 
-export class ControlFlowStatementsVisitors extends FemaScriptFormatterVisitor {}
+export class IterationStatementsVisitors
+  extends FemaScriptFormatterVisitor
+  implements cst.IterationStatementsVisitor
+{
+  iterationStatements(ctx: cst.IterationStatementsCstContext) {
+    const { doWhileStatement, forLoopStatement, whileDoStatement, ...res } =
+      ctx;
+
+    if (Object.keys(res).length > 0)
+      throw new Error("Unimplemented statement: " + JSON.stringify(res));
+
+    return [
+      this.visit(doWhileStatement),
+      this.visit(whileDoStatement),
+      this.visit(forLoopStatement),
+    ];
+  }
+}
