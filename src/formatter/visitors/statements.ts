3b83bd1fd56d3fd475ff1ad0432971e9910077b4
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 02:38:23 -0300

    feat: ✨ adicionar visitor para os statements

--- src/formatter/visitors/statements.ts
+++ src/formatter/visitors/statements.ts
@@ -1,3 +1,15 @@
+import * as cst from "@/types/cst";
 import { FemaScriptFormatterVisitor } from "../formatter";
 
-export class StatementsVisitors extends FemaScriptFormatterVisitor {}
+export class StatementsVisitors
+  extends FemaScriptFormatterVisitor
+  implements cst.StatementVisitor
+{
+  statement(ctx: cst.StatementCstContext) {
+    return [
+      this.visit(ctx.assignmentStatement),
+      this.visit(ctx.controlStatements),
+      this.visit(ctx.operationsStatements),
+    ];
+  }
+}
