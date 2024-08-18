fd7cd48a201618d124834208d49caf1dd0323f71
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 20:00:35 -0300

    feat: ✨ adicionar visitor para os statements de de `faça enquanto` (do while)

--- src/formatter/visitors/statements-control-flow.ts
+++ src/formatter/visitors/statements-control-flow.ts
@@ -1,9 +1,16 @@
 import * as cst from "@/types/cst";
+import { imageFrom } from "@/utils";
 import { FemaScriptFormatterVisitor } from "../formatter";
+import { BRK_ALLMAN, BRK_COMPACT } from "../rules/breaklines";
+import { D_INDT_COMPACT, I_INDT_COMPACT } from "../rules/indentation";
+import { WS, WS_ALLMAN, WS_COMPACT, WS_KR } from "../rules/whitespaces";
 
 export class IterationStatementsVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.IterationStatementsVisitor
+  implements
+    cst.IterationStatementsVisitor,
+    cst.DoWhileStatementVisitor,
+    cst.WhileDoStatementVisitor
 {
   iterationStatements(ctx: cst.IterationStatementsCstContext) {
     const { doWhileStatement, forLoopStatement, whileDoStatement, ...res } =
@@ -16,6 +23,30 @@ export class IterationStatementsVisitors
       this.visit(doWhileStatement),
       this.visit(whileDoStatement),
       this.visit(forLoopStatement),
+    ];
+  }
+
+  doWhileStatement(ctx: cst.DoWhileStatementCstContext) {
+    const { Do, block, While, expression } = ctx;
+
+    return [
+      [imageFrom(Do), [WS_KR, WS_COMPACT]],
+      this.visit(block),
+      [[WS_KR], [BRK_ALLMAN, BRK_COMPACT], imageFrom(While)],
+      [WS, ["(", this.visit(expression), ")", ";"]],
+    ];
+  }
+
+  whileDoStatement(ctx: cst.WhileDoStatementCstContext) {
+    const { While, expression, Do, block } = ctx;
+
+    return [
+      [imageFrom(While), WS],
+      ["(", this.visit(expression), ")", [WS_KR, WS_ALLMAN]],
+      [I_INDT_COMPACT, BRK_COMPACT],
+      [imageFrom(Do), [WS_KR, WS_COMPACT]],
+      this.visit(block),
+      [D_INDT_COMPACT],
     ];
   }
 }
