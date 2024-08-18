4f353bf44da03eecd3ec6c00d5537e43a1bf4a3d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 04:57:07 -0300

    refactor: 🎨 refator regras de quebras e pulos de linhas
    Quando uma linha é quebrada ou pulada deve-se indentar a linha posterior
    
    Haverá efeitos colaterais dessa alteração que serão corrigidos no próximo commit

--- src/formatter/rules/breaklines.ts
+++ src/formatter/rules/breaklines.ts
@@ -1,13 +1,24 @@
 import { RULE, whenAllman, whenCompact, whenKR } from "../utils/rules";
+import { INDT, INDT_ALLMAN, INDT_COMPACT, INDT_KR } from "./indentation";
 
-export const BRK_LN = "\n";
+const BREAK_LINE_CHAR = "\n";
+export const BRK_LN = [BREAK_LINE_CHAR, INDT];
 
-export const BRK_KR = RULE((opts) => whenKR(BRK_LN, opts));
-export const BRK_ALLMAN = RULE((opts) => whenAllman(BRK_LN, opts));
-export const BRK_COMPACT = RULE((opts) => whenCompact(BRK_LN, opts));
+const BRK_WHEN_KR = RULE((opts) => whenKR(BREAK_LINE_CHAR, opts));
+const BRK_WHEN_ALLMAN = RULE((opts) => whenAllman(BREAK_LINE_CHAR, opts));
+const BRK_WHEN_COMPACT = RULE((opts) => whenCompact(BREAK_LINE_CHAR, opts));
 
-export const SKIP_LN = "\n\n";
+export const BRK_KR = [BRK_WHEN_KR, INDT_KR];
+export const BRK_ALLMAN = [BRK_WHEN_ALLMAN, INDT_ALLMAN];
+export const BRK_COMPACT = [BRK_WHEN_COMPACT, INDT_COMPACT];
 
-export const SKIP_KR = RULE((opts) => whenKR(SKIP_LN, opts));
-export const SKIP_ALLMAN = RULE((opts) => whenAllman(SKIP_LN, opts));
-export const SKIP_COMPACT = RULE((opts) => whenCompact(SKIP_LN, opts));
+const SKIP_LINE_CHAR = "\n\n";
+export const SKIP_LN = [SKIP_LINE_CHAR, INDT];
+
+const SKIP_WHEN_KR = RULE((opts) => whenKR(SKIP_LINE_CHAR, opts));
+const SKIP_WHEN_ALLMAN = RULE((opts) => whenAllman(SKIP_LINE_CHAR, opts));
+const SKIP_WHEN_COMPACT = RULE((opts) => whenCompact(SKIP_LINE_CHAR, opts));
+
+export const SKIP_KR = [SKIP_WHEN_KR, BRK_KR];
+export const SKIP_ALLMAN = [SKIP_WHEN_ALLMAN, BRK_ALLMAN];
+export const SKIP_COMPACT = [SKIP_WHEN_COMPACT, BRK_COMPACT];
