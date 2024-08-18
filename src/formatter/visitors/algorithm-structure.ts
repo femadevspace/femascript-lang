c14b75d1d15e6f0deedd6c7c6898c86989b9ae8a
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 00:14:48 -0300

    feat: ✨ adicionar visitor para o cabeçalho

--- src/formatter/visitors/algorithm-structure.ts
+++ src/formatter/visitors/algorithm-structure.ts
@@ -1,11 +1,13 @@
 import * as cst from "@/types/cst";
+import { imageFrom } from "@/utils";
 import { FemaScriptFormatterVisitor } from "../formatter";
 import { SKIP_LN } from "../rules/breaklines";
+import { WS } from "../rules/whitespaces";
 import { separateWith } from "../utils/rules";
 
 export class AlgorithmStructureVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.AlgorithmVisitor
+  implements cst.AlgorithmVisitor, cst.HeaderVisitor
 {
   algorithm(ctx: cst.AlgorithmCstContext) {
     return separateWith(SKIP_LN, [
@@ -15,5 +17,15 @@ export class AlgorithmStructureVisitors
       this.visit(ctx.variablesDeclarators),
       this.visit(ctx.program),
     ]);
+  }
+
+  header(ctx: cst.HeaderCstContext) {
+    const { Algorithm, Identifier, SemiColon } = ctx;
+    return [
+      imageFrom(Algorithm),
+      WS,
+      imageFrom(Identifier),
+      imageFrom(SemiColon),
+    ];
   }
 }
