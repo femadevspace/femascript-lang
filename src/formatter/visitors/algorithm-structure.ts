7425fb0ef10f0d173d809844d6e5e8ca67ccc892
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 00:42:09 -0300

    feat: ✨ adicionar visitor para as declarações de tipos

--- src/formatter/visitors/algorithm-structure.ts
+++ src/formatter/visitors/algorithm-structure.ts
@@ -1,13 +1,17 @@
 import * as cst from "@/types/cst";
 import { imageFrom } from "@/utils";
 import { FemaScriptFormatterVisitor } from "../formatter";
-import { SKIP_LN } from "../rules/breaklines";
+import { BRK_LN, SKIP_LN } from "../rules/breaklines";
+import { D_INDT, I_INDT, INDT } from "../rules/indentation";
 import { WS } from "../rules/whitespaces";
-import { separateWith } from "../utils/rules";
+import { beforeEach, separateWith } from "../utils/rules";
 
 export class AlgorithmStructureVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.AlgorithmVisitor, cst.HeaderVisitor
+  implements
+    cst.AlgorithmVisitor,
+    cst.HeaderVisitor,
+    cst.TypesDeclaratorsVisitor
 {
   algorithm(ctx: cst.AlgorithmCstContext) {
     return separateWith(SKIP_LN, [
@@ -26,6 +30,17 @@ export class AlgorithmStructureVisitors
       WS,
       imageFrom(Identifier),
       imageFrom(SemiColon),
+    ];
+  }
+
+  typesDeclarators(ctx: cst.TypesDeclaratorsCstContext) {
+    const { Type, enumDeclarator } = ctx;
+
+    return [
+      imageFrom(Type),
+      [BRK_LN, I_INDT],
+      separateWith(BRK_LN, beforeEach(this.visit(enumDeclarator), INDT)),
+      [D_INDT],
     ];
   }
 }
