1ceb3351f60a46c301e792bb88333db1407edb9a
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 00:14:05 -0300

    refactor: ♻️ acrescentar pseudônimo na importação dos tipos da CST

--- src/formatter/visitors/algorithm-structure.ts
+++ src/formatter/visitors/algorithm-structure.ts
@@ -1,13 +1,13 @@
-import { AlgorithmCstContext, AlgorithmVisitor } from "@/types/cst";
+import * as cst from "@/types/cst";
 import { FemaScriptFormatterVisitor } from "../formatter";
 import { SKIP_LN } from "../rules/breaklines";
 import { separateWith } from "../utils/rules";
 
 export class AlgorithmStructureVisitors
   extends FemaScriptFormatterVisitor
-  implements AlgorithmVisitor
+  implements cst.AlgorithmVisitor
 {
-  algorithm(ctx: AlgorithmCstContext) {
+  algorithm(ctx: cst.AlgorithmCstContext) {
     return separateWith(SKIP_LN, [
       this.visit(ctx.header),
       this.visit(ctx.typesDeclarators),
