a1db6e89d38749df7cb931518cc1e98cecf82074
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-17 23:17:28 -0300

    feat: ✨ adicionar função para mixar os visitadores na classe principal `FemaScriptFormatterVisitor`

--- src/formatter/formatter-helper.ts
+++ src/formatter/formatter-helper.ts
@@ -3,6 +3,26 @@ import { DeepPartial, flatten, mergeDeep } from "@/utils/nested";
 import { FemaScriptFormatterVisitor } from "./formatter";
 import { defaultSettings, Settings } from "./settings";
 import { createIndentationState } from "./utils/indentations";
+import * as visitors from "./visitors";
+
+const mixInMethods = () => {
+  for (const visitor of Object.values(visitors)) {
+    Object.getOwnPropertyNames(visitor.prototype)
+      .filter(
+        (method) =>
+          method !== "constructor" &&
+          method !== "visit" &&
+          method !== "validateVisitor"
+      )
+      .forEach((method) => {
+        // @ts-ignore
+        FemaScriptFormatterVisitor.prototype[method] =
+          // @ts-ignore
+          visitor.prototype[method];
+      });
+  }
+};
+mixInMethods();
 
 export const format = (input: string, settings: DeepPartial<Settings> = {}) => {
   const options = mergeDeep(defaultSettings, settings);
