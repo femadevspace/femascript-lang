27eec232810f3343bfaf2915276993f9cf54791d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-18 00:12:18 -0300

    refactor: ♻️ acrescentar separadores na formatação do visitor `algorithm`

--- src/formatter/visitors/algorithm-structure.ts
+++ src/formatter/visitors/algorithm-structure.ts
@@ -1,17 +1,19 @@
 import { AlgorithmCstContext, AlgorithmVisitor } from "@/types/cst";
 import { FemaScriptFormatterVisitor } from "../formatter";
+import { SKIP_LN } from "../rules/breaklines";
+import { separateWith } from "../utils/rules";
 
 export class AlgorithmStructureVisitors
   extends FemaScriptFormatterVisitor
   implements AlgorithmVisitor
 {
   algorithm(ctx: AlgorithmCstContext) {
-    return [
+    return separateWith(SKIP_LN, [
       this.visit(ctx.header),
       this.visit(ctx.typesDeclarators),
       this.visit(ctx.constantsDeclarators),
       this.visit(ctx.variablesDeclarators),
       this.visit(ctx.program),
-    ];
+    ]);
   }
 }
