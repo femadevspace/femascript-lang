985a9de4fee0fc526559e004792411f5a54b8bc1
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-19 11:23:16 -0300

    feat: ✨ adicionar visitor de acesso a arranjos

--- src/formatter/visitors/miscellaneous.ts
+++ src/formatter/visitors/miscellaneous.ts
@@ -6,7 +6,11 @@ import { groupStatements } from "../utils/group-statements";
 
 export class MiscellaneousVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.BlockVisitor, cst.VariableAccessVisitor
+  implements
+    cst.BlockVisitor,
+    cst.VariableAccessVisitor,
+    cst.ArrayAccessSuffixVisitor,
+    cst.ArrayAccessVisitor
 {
   block(ctx: cst.BlockCstContext) {
     return BLOCK(groupStatements(ctx.statement, this));
@@ -16,5 +20,15 @@ export class MiscellaneousVisitors
     const { Identifier, arrayAccessSuffix } = ctx;
 
     return [imageFrom(Identifier), this.visit(arrayAccessSuffix)];
+  }
+
+  arrayAccessSuffix(ctx: cst.ArrayAccessSuffixCstContext) {
+    return [this.visit(ctx.arrayAccess)];
+  }
+
+  arrayAccess(ctx: cst.ArrayAccessCstContext) {
+    const { Identifier, NumberLiteral } = ctx;
+
+    return ["[", [imageFrom(Identifier), imageFrom(NumberLiteral)], "]"];
   }
 }
