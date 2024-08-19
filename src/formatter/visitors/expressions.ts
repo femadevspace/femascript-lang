0c2f9f67ac3d90a05e2db8b38b1d59fe9e0f78e4
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-19 11:41:54 -0300

    feat: ✨ adicionar visitor para expressão de atribuição

--- src/formatter/visitors/expressions.ts
+++ src/formatter/visitors/expressions.ts
@@ -1,10 +1,27 @@
 import * as cst from "@/types/cst";
+import { imageFrom } from "@/utils";
 import { FemaScriptFormatterVisitor } from "../formatter";
+import { WS } from "../rules/whitespaces";
 
 export class ExpressionsVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.ExpressionVisitor
+  implements cst.AssignmentExpressionVisitor, cst.ExpressionVisitor
 {
+  assignmentExpression(ctx: cst.AssignmentExpressionCstContext) {
+    const {
+      variableAccess,
+      UnarySuffixOperator,
+      AssignmentOperator,
+      expression,
+    } = ctx;
+
+    const assignment = UnarySuffixOperator
+      ? imageFrom(UnarySuffixOperator)
+      : [WS, imageFrom(AssignmentOperator), WS, this.visit(expression)];
+
+    return [this.visit(variableAccess), assignment];
+  }
+
   expression(ctx: cst.ExpressionCstContext) {
     const { ternaryExpression, ...res } = ctx;
 
