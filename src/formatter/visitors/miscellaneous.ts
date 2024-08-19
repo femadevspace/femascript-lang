8fc25201fef710791290febd7f6f26371cc691f3
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-19 09:32:26 -0300

    refactor: ♻️ refatorar visitor de bloco para implementar o utilitário de formatação de blocos

--- src/formatter/visitors/miscellaneous.ts
+++ src/formatter/visitors/miscellaneous.ts
@@ -1,13 +1,6 @@
 import * as cst from "@/types/cst";
 import { FemaScriptFormatterVisitor } from "../formatter";
-import { BRK_ALLMAN, BRK_KR, BRK_LN } from "../rules/breaklines";
-import {
-  D_INDT_ALLMAN,
-  D_INDT_KR,
-  I_INDT_ALLMAN,
-  I_INDT_KR,
-} from "../rules/indentation";
-import { WS_COMPACT } from "../rules/whitespaces";
+import { BLOCK } from "../rules/block";
 import { groupStatements } from "../utils/group-statements";
 
 export class MiscellaneousVisitors
@@ -15,16 +8,6 @@ export class MiscellaneousVisitors
   implements cst.BlockVisitor
 {
   block(ctx: cst.BlockCstContext) {
-    return [
-      [
-        [BRK_ALLMAN],
-        "{",
-        [WS_COMPACT],
-        [I_INDT_KR, BRK_KR],
-        [I_INDT_ALLMAN, BRK_ALLMAN],
-      ],
-      groupStatements(ctx.statement, this),
-      [[D_INDT_KR, D_INDT_ALLMAN], [BRK_LN], "}"],
-    ];
+    return BLOCK(groupStatements(ctx.statement, this));
   }
 }
