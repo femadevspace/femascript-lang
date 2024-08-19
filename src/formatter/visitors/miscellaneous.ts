0ed795f286c140b37f28e5c706092c772d487a17
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-19 11:22:57 -0300

    feat: ✨ adicionar visitor de acesso a variáveis

--- src/formatter/visitors/miscellaneous.ts
+++ src/formatter/visitors/miscellaneous.ts
@@ -1,13 +1,20 @@
 import * as cst from "@/types/cst";
+import { imageFrom } from "@/utils";
 import { FemaScriptFormatterVisitor } from "../formatter";
 import { BLOCK } from "../rules/block";
 import { groupStatements } from "../utils/group-statements";
 
 export class MiscellaneousVisitors
   extends FemaScriptFormatterVisitor
-  implements cst.BlockVisitor
+  implements cst.BlockVisitor, cst.VariableAccessVisitor
 {
   block(ctx: cst.BlockCstContext) {
     return BLOCK(groupStatements(ctx.statement, this));
+  }
+
+  variableAccess(ctx: cst.VariableAccessCstContext) {
+    const { Identifier, arrayAccessSuffix } = ctx;
+
+    return [imageFrom(Identifier), this.visit(arrayAccessSuffix)];
   }
 }
