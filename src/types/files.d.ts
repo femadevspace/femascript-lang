42a2e8358dcee6f728f823c27eec430734b6d59b
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-22 20:43:34 -0300

    feat: 🏷️ adicionar declaração de módulo para as extensões de arquivos da linguagem

---
+++ src/types/files.d.ts
@@ -0,0 +1,26 @@
+declare namespace FemaScriptFile {
+  declare module "*.alg" {
+    const content: string;
+    export default content;
+  }
+
+  declare module "*.algoritmo" {
+    const content: string;
+    export default content;
+  }
+
+  declare module "*.algorithm" {
+    const content: string;
+    export default content;
+  }
+
+  declare module "*.fema" {
+    const content: string;
+    export default content;
+  }
+
+  declare module "*.femascript" {
+    const content: string;
+    export default content;
+  }
+}
