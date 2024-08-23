793d7476baec1682bf1faac7435a7326eb0884b1
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-23 01:48:27 -0300

    refactor: ♻️ adicionar opção de adicionar espaços ao final do parênteses

--- src/formatter/rules/parentheses.ts
+++ src/formatter/rules/parentheses.ts
@@ -1,9 +1,16 @@
 import { imageFrom } from "@/utils";
 import { IToken } from "chevrotain";
 import { VisitedNode } from "../formatter";
+import { NONE, WS_ALLMAN, WS_KR } from "./whitespaces";
 
 export const PARENS = (
   leftParen: IToken[],
   content: VisitedNode,
-  rightParen: IToken[]
-) => [imageFrom(leftParen), content, imageFrom(rightParen)];
+  rightParen: IToken[],
+  trailingSpace = true
+) => [
+  imageFrom(leftParen),
+  content,
+  imageFrom(rightParen),
+  trailingSpace ? [WS_KR, WS_ALLMAN] : NONE,
+];
