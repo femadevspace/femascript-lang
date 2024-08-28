fda510afff92328eadae7dfc18f0957e769250cd
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-08-28 19:32:39 -0300

    refactor: ♻️ refatorar função de formatação para utilizar a função `withDefault`

--- src/formatter/formatter-helper.ts
+++ src/formatter/formatter-helper.ts
@@ -1,8 +1,8 @@
 import { parse, Production } from "@/grammar";
-import { DeepPartial, flatten, mergeDeep } from "@/utils/nested";
+import { DeepPartial, flatten } from "@/utils/nested";
 import { FemaScriptFormatterVisitor } from "./formatter";
 import { NONE } from "./rules/whitespaces";
-import { defaultSettings, Settings } from "./settings";
+import { Settings, withDefaults } from "./settings";
 import { createIndentationState } from "./utils/indentations";
 import * as visitors from "./visitors";
 
@@ -30,7 +30,7 @@ export const format = (
   settings: DeepPartial<Settings> = {},
   entryPoint: Production = "algorithm"
 ) => {
-  const options = mergeDeep(defaultSettings, settings);
+  const options = withDefaults(settings);
   const indentState = createIndentationState();
 
   const cst = parse(input, entryPoint);
