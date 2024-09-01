06c3a81175f2f99ceda2ebfc0250e064eec93448
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-01 12:21:54 -0300

    feat: ✨ adicionar definições de constantes globais

---
+++ src/utils/globals.ts
@@ -0,0 +1,13 @@
+import type { DocumentSelector } from "vscode";
+
+export const LANGUAGE_NAME = "FemaScript";
+export const LANGUAGE_ID = "femascript";
+export const LANGUAGE_SELECTOR: DocumentSelector = { language: LANGUAGE_ID };
+
+export const FILE_EXTENSIONS = [
+  "alg",
+  "algoritmo",
+  "algorithm",
+  "fema",
+  "femascript",
+];
