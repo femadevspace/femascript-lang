6c5e522d2e142a6276287aae0e12491830c94bec
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-01 15:12:29 -0300

    refactor: ♻️ adicionar `ativador` e `desativador`  de recursos (features) da extensão

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,12 @@
+import * as features from "@/features";
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  Object.values(features).forEach((feature) =>
+    context.subscriptions.push(feature.register())
+  );
+}
 
-export function deactivate() {}
+export function deactivate() {
+  features.diagnostics.disposeDiagnosticCollection();
+}
