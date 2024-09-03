4a36ecb48a4a90cf2d8dfd2b5bb6a9bcc002afa1
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-03 12:12:40 -0300

    refactor: 🚚 mover configuração declarativa de linguagem para o diretório `fixtures`

--- scripts/prepublish/index.ts
+++ scripts/prepublish/index.ts
@@ -1,10 +1,8 @@
 import { generateDts } from "./generate-dts";
 import { generateHighlightCode } from "./generate-highlight-grammar";
-import { generateLanguageSettings } from "./generate-language-settings";
 
 await Promise.all([
   Bun.write("./src/types/cst.d.ts", generateDts()),
-  Bun.write("./extension/language-settings.json", generateLanguageSettings()),
   Bun.write(
     "./extension/highlight/femascript-grammar.tmLanguage.json",
     generateHighlightCode()
