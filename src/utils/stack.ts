5c6752e685f6ccaa4760fb0d999936e042844670
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-05 13:39:53 -0300

    feat: ✨ adicionar utilitário de `pilha` (stack)

---
+++ src/utils/stack.ts
@@ -0,0 +1,27 @@
+export class Stack<T> {
+  private stack: T[] = [];
+
+  push(item: T) {
+    this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  size() {
+    return this.stack.length;
+  }
+
+  isEmpty() {
+    return this.stack.length === 0;
+  }
+
+  clear() {
+    this.stack = [];
+  }
+}
