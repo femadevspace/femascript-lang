86ef89142a5ed76fb0b60dfdaa7d09dd00f9eb6d
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-06 00:23:51 -0300

    feat: ✨ adicionar regras que forçam ao menos um nível de indentação após quebrarem uma linha

--- src/formatter/rules/breaklines.ts
+++ src/formatter/rules/breaklines.ts
@@ -1,8 +1,18 @@
 import { RULE, whenAllman, whenCompact, whenKR } from "../utils/rules";
-import { INDT, INDT_ALLMAN, INDT_COMPACT, INDT_KR } from "./indentation";
+import {
+  F_INDT,
+  F_INDT_ALLMAN,
+  F_INDT_COMPACT,
+  F_INDT_KR,
+  INDT,
+  INDT_ALLMAN,
+  INDT_COMPACT,
+  INDT_KR,
+} from "./indentation";
 
 const BREAK_LINE_CHAR = "\n";
 export const BRK_LN = [BREAK_LINE_CHAR, INDT];
+export const F_BRK_LN = [BREAK_LINE_CHAR, F_INDT];
 
 const BRK_WHEN_KR = RULE((opts) => whenKR(BREAK_LINE_CHAR, opts));
 const BRK_WHEN_ALLMAN = RULE((opts) => whenAllman(BREAK_LINE_CHAR, opts));
@@ -11,6 +21,10 @@ const BRK_WHEN_COMPACT = RULE((opts) => whenCompact(BREAK_LINE_CHAR, opts));
 export const BRK_KR = [BRK_WHEN_KR, INDT_KR];
 export const BRK_ALLMAN = [BRK_WHEN_ALLMAN, INDT_ALLMAN];
 export const BRK_COMPACT = [BRK_WHEN_COMPACT, INDT_COMPACT];
+
+export const F_BRK_KR = [BRK_WHEN_KR, F_INDT_KR];
+export const F_BRK_ALLMAN = [BRK_WHEN_ALLMAN, F_INDT_ALLMAN];
+export const F_BRK_COMPACT = [BRK_WHEN_COMPACT, F_INDT_COMPACT];
 
 const SKIP_RULE = RULE((opts, params) =>
   opts.indentation.keepBetweenLines ? `\n${INDT(opts, params)}\n` : "\n\n"
