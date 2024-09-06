6ae3a11ec26be15e000a1e914afaf9eda79c92f8
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-06 00:22:05 -0300

    feat: ✨ adicionar regra os `cases` (quando) do bloco `switch` (avalie)

--- src/formatter/rules/block.ts
+++ src/formatter/rules/block.ts
@@ -1,16 +1,19 @@
+import type { BlockCstNode, StatementCstNode } from "@/types/cst";
 import { imageFrom } from "@/utils";
 import { IToken } from "chevrotain";
-import type { VisitedNode } from "../formatter";
-import { BRK_ALLMAN, BRK_KR, BRK_LN } from "./breaklines";
+import type { FemaScriptFormatterVisitor, VisitedNode } from "../formatter";
+import { BRK_ALLMAN, BRK_KR, BRK_LN, F_BRK_COMPACT } from "./breaklines";
 import {
   D_INDT_ALLMAN,
+  D_INDT_COMPACT,
   D_INDT_KR,
   I_INDT_ALLMAN,
+  I_INDT_COMPACT,
   I_INDT_KR,
   POP_ALIGN_COMPACT,
   PUSH_ALIGN_COMPACT,
 } from "./indentation";
-import { WS_COMPACT } from "./whitespaces";
+import { NONE, WS, WS_ALLMAN, WS_COMPACT, WS_KR } from "./whitespaces";
 
 export const BLOCK = (
   leftCurly: IToken[],
@@ -33,3 +36,45 @@ export const BLOCK = (
     [POP_ALIGN_COMPACT],
   ],
 ];
+
+/**
+ * Used for the `case` and `default` keyword
+ * in a `switch` statement.
+ */
+export const CASE = (
+  doToken: IToken[],
+  colonToken: IToken[] | undefined,
+  blockNode: BlockCstNode[] | undefined,
+  statementNode: StatementCstNode[] | undefined,
+  visitor: FemaScriptFormatterVisitor
+): VisitedNode => {
+  const isAssignmentStatement =
+    !!statementNode?.at(0)?.children.assignmentStatement;
+
+  const behaviorAfterColon = isAssignmentStatement
+    ? [WS]
+    : [BRK_KR, BRK_ALLMAN, [WS_COMPACT, PUSH_ALIGN_COMPACT]];
+
+  const behaviorAfterStatement = isAssignmentStatement
+    ? NONE
+    : POP_ALIGN_COMPACT;
+
+  const withColon = [
+    imageFrom(colonToken),
+    behaviorAfterColon,
+    visitor.visit(statementNode),
+    behaviorAfterStatement,
+  ];
+
+  const withBlock = [WS_KR, WS_COMPACT, visitor.visit(blockNode)];
+
+  const content = statementNode ? withColon : withBlock;
+
+  return [
+    [WS_KR, WS_ALLMAN],
+    [I_INDT_COMPACT, F_BRK_COMPACT],
+    imageFrom(doToken),
+    content,
+    [D_INDT_COMPACT],
+  ];
+};
