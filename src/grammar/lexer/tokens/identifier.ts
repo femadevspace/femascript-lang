81156db9d7056bd2bb5ad2df536d746847b801f2
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-06 14:48:45 -0300

    fix: 🐛 adicionar token `Identifier` na categoria de `PrimitiveTypes`
    Issue: #6

--- src/grammar/lexer/tokens/identifier.ts
+++ src/grammar/lexer/tokens/identifier.ts
@@ -1,7 +1,9 @@
 import { createAndRegisterToken as TOKEN } from "../tokens-registry";
+import { PrimitiveTypes } from "./types";
 
 export const Identifier = TOKEN({
   name: "Identifier",
   pattern: /[a-zA-Z$_][a-zA-Z0-9$_]*/,
   textmateScope: "variable.other",
+  categories: [PrimitiveTypes],
 });
