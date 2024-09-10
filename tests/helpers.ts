a82ecab5e597d547838712637de2b5b7861c2f2c
Thiago Ausechi <thiago_ausechi@icloud.com>
2024-09-10 00:47:55 -0300

    test: 🧪 adicionar utilitário de iteração para cada estilo de formatação

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,5 +1,21 @@
+import { FormattingStyle } from "@/formatter";
 import fs from "fs";
 import path from "path";
 
 export const loadFixture = (filePath: string) =>
   fs.readFileSync(path.resolve(__dirname, "fixtures/", filePath), "utf-8");
+
+type CallbackParams = {
+  styleName: string;
+  style: FormattingStyle;
+  fileName: string;
+};
+
+export const forEachStyle = (callback: (params: CallbackParams) => void) =>
+  (["K&R", "Allman", "Compact"] as const).forEach((style) =>
+    callback({
+      styleName: style.concat(" style"),
+      style: style.toLowerCase() as FormattingStyle,
+      fileName: `expected_${style.toLowerCase().replace("&", "")}.alg`,
+    })
+  );
